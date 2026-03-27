00854031791469ac5378928b681c927866983f0e
Raul <rauldumitras75@gmail.com>
2026-03-27 10:45:37 +0200

    feat: auth Guard - protejare rute private

--- Client/src/app/app.routes.ts
+++ Client/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { Joburi } from './components/joburi/joburi';
 import { RegisterComponent } from './components/register/register';
+import { authGuard } from './auth-guard';
 
 export const routes: Routes = [
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'joburi', component: Joburi}
+    {path: 'joburi', component: Joburi, canActivate:[authGuard]}
 ];
