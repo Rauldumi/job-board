4971d8d0155af064cefbf5cdf654ab864e198bd5
Raul <rauldumitras75@gmail.com>
2026-03-27 11:17:07 +0200

    feat: logout - logout button si logica in navbar

--- Client/src/app/app.ts
+++ Client/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { AuthService } from './services/auth';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink],
@@ -8,5 +9,10 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Client');
-}
+  constructor(private authService: AuthService, private router: Router) {}
+
+  logout() { 
+    this.authService.logout();
+    this.router.navigate(['/login'])
+    }
+  }
