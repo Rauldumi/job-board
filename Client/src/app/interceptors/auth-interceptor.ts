2c758ceb8fb1797084db906e777a958a57de13f5
Raul <rauldumitras75@gmail.com>
2026-03-27 11:07:03 +0200

    feat: auth interceptor - cod auth.interceptor.ts

--- Client/src/app/interceptors/auth-interceptor.ts
+++ Client/src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const userLogat = localStorage.getItem('userLogat');
+  
+  if(userLogat) {
+    const reqUserLogat = req.clone({
+        setHeaders: {
+        Authorization : `Bearer ${userLogat}`
+      }
+    });
+    return next(reqUserLogat);
+  }
   return next(req);
 };
